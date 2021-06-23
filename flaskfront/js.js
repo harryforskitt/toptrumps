d69f0f1ef50a8e346000306128a00bdf2ea63c6e
harryforskitt <83458028+harryforskitt@users.noreply.github.com>
2021-06-23 13:00:06 +0100

    Added card counters

--- flaskfront/js.js
+++ flaskfront/js.js
@@ -37,6 +37,8 @@ function playCard(attribute, player){
 		// There was an error
 		console.warn('Something went wrong.', err);
 	})
+	computerCards();
+	playerCards();
 };
 
 function computerPlay(){
@@ -51,6 +53,46 @@ function computerPlay(){
 		// This is the JSON from our response
 		console.log(data);
 		document.getElementById("winner").innerHTML = data;
+		return(data);
+	}).catch(function (err) {
+		// There was an error
+		console.warn('Something went wrong.', err);
+	})
+	computerCards();
+	playerCards();
+};
+
+function playerCards(){
+	var player = '0';
+	var attribute = getRandomInt(5);
+	url="http://harryforskitt.com:5000//playercards/0";
+	console.log(url)
+	fetch(url).then(function (response) {
+		// The API call was successful!
+		return response.text();
+	}).then(function (data) {
+		// This is the JSON from our response
+		console.log(data);
+		document.getElementById("playerCards").innerHTML = data;
+		return(data);
+	}).catch(function (err) {
+		// There was an error
+		console.warn('Something went wrong.', err);
+	})
+};
+
+function computerCards(){
+	var player = '0';
+	var attribute = getRandomInt(5);
+	url="http://harryforskitt.com:5000//playercards/1";
+	console.log(url)
+	fetch(url).then(function (response) {
+		// The API call was successful!
+		return response.text();
+	}).then(function (data) {
+		// This is the JSON from our response
+		console.log(data);
+		document.getElementById("computerCards").innerHTML = data;
 		return(data);
 	}).catch(function (err) {
 		// There was an error
